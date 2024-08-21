4add31081ed0321f9281c67bd43894ee91842b79
jay1106-tm <manojkr.mk878@gmail.com>
2024-08-21 11:32:03 +0530

    Week_4_Assignment

---
+++ Week_4/BookstoreAPI/src/main/java/springfox/documentation/builders/PathSelectors.java
@@ -0,0 +1,9 @@
+package springfox.documentation.builders;
+
+public class PathSelectors {
+
+	public static Object any() {
+		return null;
+	}
+
+}
--- Week_4/BookstoreAPI/src/test/java/com/example/BookstoreAPI/BookstoreApiApplicationTests.java
+++ Week_4/BookstoreAPI/src/test/java/com/example/BookstoreAPI/BookstoreApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.BookstoreAPI;
+package com.example.bookstoreapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
